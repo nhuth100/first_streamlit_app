50b159a73537cf83059757310e266b1bfb218176
nhuth100 <125407653+nhuth100@users.noreply.github.com>
2023-02-21 22:03:32 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ def get_fruityvice_data(this_fruit_choice):
      return fruityvice_normalized
 
 
-streamlit.header("the fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
