fcf34d285a878e326ea1298fe1b3b57541ed2a63
nhuth100 <125407653+nhuth100@users.noreply.github.com>
2023-02-21 20:09:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,12 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
+streamlit.write('The user entered ', add_my_fruit)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+
 
 
 # take the json version of the response and normalize it
