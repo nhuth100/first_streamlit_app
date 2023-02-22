e94ca704d229d371a0e3f7ec2028e202f0544184
nhuth100 <125407653+nhuth100@users.noreply.github.com>
2023-02-21 20:13:13 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
 streamlit.write('thanks for adding ', add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
